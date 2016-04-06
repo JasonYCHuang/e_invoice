a4df5c1c44d62ad56066115e8a91285ef21a0cfa
JasonYCH <ychuang1981@gmail.com>
2016-04-06 16:34:17 +0200

    modify lib/e_invoice.rb

--- lib/e_invoice.rb
+++ lib/e_invoice.rb
@@ -1,5 +1,8 @@
 require "e_invoice/version"
-
-module EInvoice
-  # Your code goes here...
-end
+require "e_invoice/result"
+require "e_invoice/duration"
+require "e_invoice/invoice"
+require "e_invoice/info"
+require "e_invoice/header"
+require "e_invoice/payload"
+require "e_invoice/configuration"
